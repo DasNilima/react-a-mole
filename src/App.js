9f0d7a9f80aeaef51675287048adbe8e0c588cc4
Niima1985 <67212610+Niima1985@users.noreply.github.com>
2022-08-08 14:26:37 -0700

    delete reset button

--- src/App.js
+++ src/App.js
@@ -4,29 +4,32 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function App(){
-    let [score, setScore] = useState(0)
-    const createMoleHill = () => {
+  let [score, setScore] = useState(0);
+  const createMoleHill = () => {
         let hills = []
       for (let i = 0; i < 9; i++) {
         hills.push(
           <MoleContainer
-            key= {i}
-            setScore = { setScore }
-            score={score} />
-            )
-        }   
-    return (
-      <div>{hills} </div>
+            key={i}
+            setScore={setScore}
+            score={score}
+        />
         )
-    }
+      };
+    return (
+      <div>{hills}
+      </div>
+      )
+  }
     return (
         <div className="App">
         <h1>React-a-Mole!</h1>
         {score}
-        <hr />
-        <button onClick={() => setScore(() => 0)}className="btn btn-danger">Reset</button>
         {createMoleHill()}
-        </div>
+      </div>
+      
     )
 }
+
+
 
--- src/components/MoleContainer.jsx
+++ src/components/MoleContainer.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Mole from './Mole'
 import EmptySlot from './EmptySlot'
 
+
 const MoleContainer = (props) => {
     let [theMole, setTheMole] = useState(false)
     const handleClick = (e) => {
